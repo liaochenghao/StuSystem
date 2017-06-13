848bfdf8e60c43290454ce29e1df40e94b75c4a1
邱雷 <qiulei@qiuleideMacBook-Pro.local>
2017-06-14 02:30:58 +0800

    修改配置文件

--- StuSystem/settings.py
+++ StuSystem/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2&x74*2^ffs6coogjrh-@7n#!c5dgd@vhw^h+)8tw(_7orzlmk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', '42.51.8.152']
 
 
 # Application definition
