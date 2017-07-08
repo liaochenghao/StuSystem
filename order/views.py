7db0f5ca9c2ba4bbbfd092b12d8b8a8aa5c15646
邱雷 <qiulei@qiuleideMacBook-Pro.local>
2017-07-09 01:33:09 +0800

    123

--- order/views.py
+++ order/views.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 from rest_framework import mixins, viewsets
 from rest_framework.decorators import list_route
-from order.models import Order, OrderPayment
+from order.models import Order, OrderPayment, UserCourse
 from order.serializers import OrderSerializer, OrderPaymentSerializer
 from rest_framework.response import Response
 
@@ -16,9 +16,11 @@ class OrderViewSet(mixins.CreateModelMixin,
 
     @list_route()
     def check_order(self, request):
-        order = self.queryset.filter(user=self.request.user, status__in=['TO_PAY', 'TO_CONFIRM']).last()
+        order = self.queryset.filter(user=self.request.user, status__in=['TO_PAY', 'TO_CONFIRM', 'CONFIRMED']).last()
         if order:
-            return Response(self.get_serializer(order).data)
+            order_course_count = UserCourse.objects.filter(order=order).count()
+            if order_course_count < order.course_num:
+                return Response(self.get_serializer(order).data)
         return Response({'code': 100, 'msg': '没有未完成的订单，可以创建'})
 
 
