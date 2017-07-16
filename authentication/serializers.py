e2c0002a824db09384d385511d61dc4fcf97f3a2
邱雷 <qiulei@qiuleideMacBook-Pro.local>
2017-07-16 21:35:39 +0800

    xiugai bug

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -42,14 +42,14 @@ class CreateAccountSerializer(serializers.Serializer):
             user.save()
 
             # 创建用户信息
-            user_info, created = UserInfo.objects.update_or_create(defaults={'openid': res['openid']},
+            student_info, created = UserInfo.objects.update_or_create(defaults={'openid': res['openid']},
                                                                    **{
                                                                     "user": user,
                                                                     "unionid": user_info.get('unionid'),
                                                                     "openid": res['openid'],
                                                                     })
-            user_info.headimgurl = user_info['headimgurl']
-            user_info.wx_name = user_info['nickname']
+            student_info.headimgurl = user_info['headimgurl']
+            student_info.wx_name = user_info['nickname']
             user_info.save()
 
             # 自动分配课程顾问
