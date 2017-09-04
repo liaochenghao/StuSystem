a21ad71978c6c1f3d675baa33fb6f7adb97927e0
邱雷 <qiulei@qiuleideMacBook-Pro.local>
2017-09-04 22:17:24 +0800

    优化销售人员创建

--- market/serializers.py
+++ market/serializers.py
@@ -13,7 +13,6 @@ class SalesManSerializer(serializers.ModelSerializer):
 
 
 class ChannelSerializer(serializers.ModelSerializer):
-    sales_man = SalesManSerializer()
 
     class Meta:
         model = Channel
@@ -26,3 +25,8 @@ class ChannelSerializer(serializers.ModelSerializer):
         instance.qr_code = get_long_qr_code('channel_id_%s' % instance.id)
         instance.save()
         return instance
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['sales_man'] = SalesManSerializer(instance=instance.sales_man).data
+        return data
