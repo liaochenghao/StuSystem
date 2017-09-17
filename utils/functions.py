bf5135dba38451ba9ad92449e8c9cc52d2aae490
邱雷 <qiulei@qiuleideMacBook-Pro.local>
2017-09-17 14:27:06 +0800

    123

--- utils/functions.py
+++ utils/functions.py
@@ -13,9 +13,9 @@ def get_key_verbose_data(data: dict):
 def get_long_qr_code(key):
     """根据key值获取永久二维码"""
     data = {
-        'action_name': 'QR_LIMIT_SCENE',
+        'action_name': 'QR_LIMIT_STR_SCENE',
         'action_info': {
-            'scene': {'scene_id': key}
+            'scene': {'scene_str': key}
         }
     }
     res = client.create_qrcode(data)
@@ -23,3 +23,18 @@ def get_long_qr_code(key):
     if res.get('url') and res.get('ticket'):
         qr_code = client.show_qrcode(res['ticket']).url
     return qr_code
+
+
+# def get_temporary_qr_code(key):
+#     """获取临时二维码，默认7天有效"""
+#     access_token =
+#     url = 'https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token=' %
+#     data = {
+#         "expire_seconds": 7 * 24 * 3600,
+#         "action_name": "QR_SCENE",
+#         "action_info": {
+#             "scene": {
+#                 "scene_id": 123
+#             }
+#         }
+#     }
