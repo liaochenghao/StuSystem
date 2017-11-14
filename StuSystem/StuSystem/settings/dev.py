525acb9739faf4b18734c8496e4e50b623f51877
邱雷 <qiulei@qiuleideMacBook-Pro.local>
2017-11-14 15:09:23 +0800

    优化学生登录

--- StuSystem/StuSystem/settings/dev.py
+++ StuSystem/StuSystem/settings/dev.py
@@ -4,28 +4,28 @@ from StuSystem.settings.common import *
 
 DEBUG = True
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'stu_system',
-#         'USER': 'root',
-#         'PASSWORD': 'qwe896275756',
-#         'HOST': '42.51.8.152',
-#         'PORT': 3306,
-#         'CHARSET': 'UTF-8',
-#         'ATOMIC_REQUESTS': True
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'stu_system',
         'USER': 'root',
-        'PASSWORD': 'svLE26eg',
-        'HOST': '47.92.115.126',
+        'PASSWORD': 'qwe896275756',
+        'HOST': '42.51.8.152',
         'PORT': 3306,
         'CHARSET': 'UTF-8',
         'ATOMIC_REQUESTS': True
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'stu_system',
+#         'USER': 'root',
+#         'PASSWORD': 'svLE26eg',
+#         'HOST': '47.92.115.126',
+#         'PORT': 3306,
+#         'CHARSET': 'UTF-8',
+#         'ATOMIC_REQUESTS': True
+#     }
+# }
