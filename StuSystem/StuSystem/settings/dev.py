dd274a4ef2cebc62c2db48637987c19488d9ff4a
QiuLei <896275756@qq.com>
2017-12-11 18:27:26 +0800

    debug

--- StuSystem/StuSystem/settings/dev.py
+++ StuSystem/StuSystem/settings/dev.py
@@ -16,6 +16,22 @@ DATABASES = {
     }
 }
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': ['/home/qiulei/workplace/summer-web-h5/'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 # mongodb配置
 MONGODB_CONFIG = {
     'host': '42.51.8.152',
