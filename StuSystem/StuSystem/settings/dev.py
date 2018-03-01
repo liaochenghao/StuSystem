5835d80e9cfbec9e62b89074f200ee861d880356
bobowang2017 <409879654@qq.com>
2018-03-01 09:30:35 +0800

    修改数据库链接地址为47.92.115.126

--- StuSystem/StuSystem/settings/dev.py
+++ StuSystem/StuSystem/settings/dev.py
@@ -8,8 +8,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'stu_system',
         'USER': 'root',
-        'PASSWORD': '1q2w3e4r!Q',
-        'HOST': '42.51.8.152',
+        'PASSWORD': 'svLE26eg',
+        'HOST': '47.92.115.126',
         'PORT': 3306,
         'CHARSET': 'UTF-8',
         'ATOMIC_REQUESTS': True
@@ -17,7 +17,7 @@ DATABASES = {
 }
 
 REDIS_CONFIG = {
-    'host': '42.51.8.152',
+    'host': '47.92.115.126',
     'port': 6379
 }
 
