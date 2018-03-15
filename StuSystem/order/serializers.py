5051ff966218b0f01c2da97373147e94c00908ef
bobowang2017 <409879654@qq.com>
2018-03-15 17:06:28 +0800

    创建初始成绩单寄送

--- StuSystem/order/serializers.py
+++ StuSystem/order/serializers.py
@@ -151,7 +151,7 @@ class OrderSerializer(serializers.ModelSerializer):
         order_payment = OrderPayment.objects.filter(order=instance).last()
         data['order_payed_info'] = OrderPaymentSerializer(order_payment).data if order_payment else None
         user_info = UserInfo.objects.get(user=instance.user)
-        logger.info('user_info: %s' % user_info.__repr__())
+        logger.info('user_info: %s' % user_info.name)
         data['user'] = {
             'id': instance.user.id,
             'name': user_info.name
