dd19bc3c37f4bdae47634aac7ad1614cd2376b15
bobowang2017 <409879654@qq.com>
2018-03-16 10:42:38 +0800

    添加微服务访问记录日志

--- StuSystem/order/serializers.py
+++ StuSystem/order/serializers.py
@@ -41,7 +41,7 @@ class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = ['id', 'user', 'chart_ids', 'currency', 'payment', 'create_time', 'modified_time', 'status',
-                  'standard_fee', 'pay_fee', 'remark', 'coupon_list', 'order_number', 'check_remark']
+                  'standard_fee', 'pay_fee', 'remark', 'coupon_list', 'order_number']
         read_only_fields = ['user', 'pay_fee', 'standard_fee', 'order_number']
 
     def validate(self, attrs):
