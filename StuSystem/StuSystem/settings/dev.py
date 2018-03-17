076cc811c9cc94538df1f1bb2de6f75be8602d10
bobowang2017 <409879654@qq.com>
2018-03-17 19:02:34 +0800

    debug

--- StuSystem/StuSystem/settings/dev.py
+++ StuSystem/StuSystem/settings/dev.py
@@ -48,7 +48,7 @@ MONGODB_CONFIG = {
 }
 
 micro_service_domain = 'http://localhost:7071'
-DOMAIN = 'http://su.chinasummer.org'
+DOMAIN = 'http://shell.chinasummer.org'
 BASE_MEDIA_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 MEDIA_ROOT = '%s/media' % BASE_MEDIA_ROOT
 
