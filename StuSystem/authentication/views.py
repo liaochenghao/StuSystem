ea0d47ee4363a04e5b91c4ea87398c3e5152e544
bobowang2017 <409879654@qq.com>
2018-03-19 19:42:57 +0800

    debug ready 001

--- StuSystem/authentication/views.py
+++ StuSystem/authentication/views.py
@@ -47,8 +47,8 @@ class UserViewSet(mixins.ListModelMixin,
         logging.info('authorize start ' + str(datetime.now()))
         return response
 
-    @list_route(['GET'],serializer_class=GetUserInfoSerializer)
-    def take_user_info(self,request):
+    @list_route(['GET'], serializer_class=GetUserInfoSerializer)
+    def take_user_info(self, request):
         print('++++++++++++++++++++++++++++++++++++++')
         logging.info('+++++++++++++++++++++++++++++++++++++++')
         serializer = self.serializer_class()
@@ -67,8 +67,8 @@ class UserViewSet(mixins.ListModelMixin,
         data['ticket'] = ticket
         serializer = self.serializer_class(data=data)
         serializer.is_valid(raise_exception=True)
-        res = serializer.check_account(serializer.validated_data,)
-        logging.info('check_account: %s' % res.__dict__)
+        res = serializer.check_account(serializer.validated_data, )
+        logging.info('check_account: ', res)
         response = Response(res)
         response.set_cookie('ticket', res.get('ticket'))
         return response
