7622f38ea2aa36e70d3cef53bce08aed9e934135
kimyeee@163.com <kimyeee@163.com>
2018-04-11 17:35:57 +0800

    修复学生优惠券显示

--- StuSystem/order/serializers.py
+++ StuSystem/order/serializers.py
@@ -105,7 +105,7 @@ class OrderSerializer(serializers.ModelSerializer):
         # coupon_list = validated_data.pop('coupon_list', None)
         chart_ids = validated_data.pop('chart_ids')
         user = self.context['request'].user
-        coupon_list = [coupon[0] for coupon in UserCoupon.objects.filter(user=user, status='TO_USE').values_list('id')]
+        coupon_list = [coupon[0] for coupon in UserCoupon.objects.filter(user=user, status='TO_USE').values_list('coupon_id')]
         validated_data = self.validated_data_additional(validated_data, chart_ids, coupon_list, user)
         order = super().create(validated_data)
         self.additional_order_update(order, coupon_list, chart_ids, user)
